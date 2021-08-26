3723611745337a9088a03d0c45dd7275c53d0a82
Gyuri <hamrangyuri@gmail.com>
2021-08-26 13:51:36 +0200

    Init

---
+++ index.ts
@@ -0,0 +1,18 @@
+import DiscordJS, { Intents } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new DiscordJS.Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+client.on("ready", () => {
+  console.log("Bot is ready!");
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content === "ping") message.reply({ content: "Pong!" });
+});
+
+client.login(process.env.BOT_TOKEN);
